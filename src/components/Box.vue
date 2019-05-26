3d6cf132064d4f75b213ef3cb8d55f5275b3fd1b
Tejas Gami <tejasgami111@gmail.com>
2019-05-26 18:15:33 +0530

    add box component

--- src/components/Box.vue
+++ src/components/Box.vue
@@ -1,72 +1,68 @@
 <template>
-  <v-layout row wrap>
-    <v-flex xs12 sm3>
-      <v-card color="primary">
-        <v-img :aspect-ratio="16/9" src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"></v-img>
-        <v-layout pa-2 column fill-height class="lightbox">
-          <v-spacer></v-spacer>
-          <v-flex shrink>
-            <div class="subheading">
-              <span>44</span>
-              <span class="align-right">2699 kd</span>
-            </div>
-            <div class="body-1">GenItm-03008</div>
-          </v-flex>
-        </v-layout>
-      </v-card>
-    </v-flex>
-    <v-flex xs12 sm9>
-      <v-card class="primary" height="100%">
-        <v-card-title>
-          <h4 class="align-self-start">{{ "#1" }}</h4>
-          <v-spacer></v-spacer>
-          <h4 class="align-self-end">{{ "Bugatti - Boty" }}</h4>
-        </v-card-title>
-        <v-divider></v-divider>
-        <v-layout row wrap>
-          <v-flex xs9>
-            <v-layout row wrap>
-              <v-flex xs2>
-                <img class="align-center pt-3 pl-3" src="../assets/icon/style-fit1.svg">
-                <v-card-text class="align-center pt-3 pl-3">asfd</v-card-text>
-              </v-flex>
-              <v-flex xs2>
-                <img class="align-center pa-3" src="../assets/icon/style-fit1.svg">
-                <v-card-text>asfd</v-card-text>
-              </v-flex>
-              <v-flex xs2>
-                <img class="align-center pa-3" src="../assets/icon/style-fit1.svg">
-                <v-card-text>asfd</v-card-text>
-              </v-flex>
-              <v-flex xs2>
-                <img class="align-center pa-3" src="../assets/icon/style-fit1.svg">
-                <v-card-text>asfd</v-card-text>
-              </v-flex>
-              <v-flex xs2>
-                <img class="align-center pa-3" src="../assets/icon/style-fit1.svg">
-                <v-card-text>asfd</v-card-text>
-              </v-flex>
-            </v-layout>
-          </v-flex>
-          <v-spacer></v-spacer>
-          <v-flex xs3>
-            <v-flex xs1>
-              <v-card-title class="heading">Kept</v-card-title>
+  <div v-if="cards.length>0" >
+    <v-layout row wrap v-for="card in cards" :key="`${card.id}`" mb-3> 
+      <v-flex xs12 sm3>
+        <v-card color="primary">
+          <v-img :aspect-ratio="16/9" src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"></v-img>
+        </v-card>
+      </v-flex>
+      <v-flex xs12 sm9>
+        <v-card class="primary" height="100%">
+          <v-card-title>
+            <h4 class="align-self-start">{{ `#${card.id}` }}</h4>
+            <v-spacer></v-spacer>
+            <h4 class="align-self-end">{{ card.name }}</h4>
+          </v-card-title>
+          <v-divider></v-divider>
+          <v-layout row wrap>
+            <v-flex xs9>
+              <v-layout row wrap>
+                <v-flex xs2>
+                  <img class="align-center pt-3 pl-3" src="../assets/icon/style-fit1.svg">
+                  <v-card-text class="align-center pt-3 pl-3">STYLE</v-card-text>
+                </v-flex>
+                <v-flex xs2>
+                  <img class="align-center pt-3 pl-3" src="../assets/icon/style-fit2.svg">
+                  <v-card-text>FIT</v-card-text>
+                </v-flex>
+                <v-flex xs2>
+                  <img class="align-center pt-3 pl-3" src="../assets/icon/price0.svg">
+                  <v-card-text>PRICE</v-card-text>
+                </v-flex>
+                <v-flex xs2>
+                  <img class="align-center pt-3 pl-3" src="../assets/icon/size0.svg">
+                  <v-card-text>SIZE</v-card-text>
+                </v-flex>
+                <v-flex xs2 v-show="card.comment">
+                  <img class="align-center pt-3 pl-3" src="../assets/icon/style-fit1.svg">
+                </v-flex>
+              </v-layout>
             </v-flex>
-            <v-flex shrink>
-              <div class="subheading">Lee</div>
-              <v-spacer></v-spacer>
-              <div class="body-1">hey</div>
+            <v-spacer></v-spacer>
+            <v-flex xs3>
+              <v-flex xs1>
+                <v-card-title class="heading">{{card.keep?"Kept":"Return"}}</v-card-title>
+              </v-flex>
+              <v-flex shrink>
+                <div class="subheading">{{card.address1}}</div>
+                <v-spacer></v-spacer>
+                <div class="body-1">{{card.address2}}</div>
+              </v-flex>
             </v-flex>
-          </v-flex>
-        </v-layout>
-      </v-card>
-    </v-flex>
-  </v-layout>
+          </v-layout>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </div>
 </template>
 <script>
 export default {
-  name: "Box"
+  name: "Box",
+  props: {
+    cards: Array
+  },
+  mounted: {},
+  
 };
 </script>
 
--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,122 +1,156 @@
 <template>
-<div>
-  <!-- <v-container fluid grid-list-xl> -->
-     <v-layout wrap align-center>
-    <v-flex xs12 sm6 d-flex pl-3 pt-3>
-        <v-select
-          :items="items"
-          box
-          label="Box style"
-        ></v-select>
-      </v-flex>
-     </v-layout>
-  <!-- </v-container> -->
-  <v-container fluid fill-height style="display:table">
-   
-    <v-layout row wrap>
-      <v-flex xs12 sm3>
-        <v-card color="primary">
-          <v-img :aspect-ratio="16/9" src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"></v-img>
-          <v-layout pa-2 column fill-height class="lightbox">
-            <v-spacer></v-spacer>
-            <v-flex shrink>
-              <div class="subheading">
-                <span>44</span>
-                <span class="align-right">2699 kd</span>
-              </div>
-              <div class="body-1">GenItm-03008</div>
-            </v-flex>
-          </v-layout>
-        </v-card>
-      </v-flex>
-      <v-flex xs12 sm9>
-        <v-card class="primary" height="100%">
-          <v-card-title>
-            <h4 class="align-self-start">{{ "#1" }}</h4>
-            <v-spacer></v-spacer>
-            <h4 class="align-self-end">{{ "Bugatti - Boty" }}</h4>
-          </v-card-title>
-          <v-divider></v-divider>
-          <v-layout row wrap>
-            <v-flex xs9>
-              <v-layout row wrap>
-                <v-flex xs2>
-                  <img class="align-center pt-3 pl-3" src="../assets/icon/style-fit1.svg">
-                  <v-card-text class="align-center pt-3 pl-3">asfd</v-card-text>
-                </v-flex>
-                <v-flex xs2>
-                  <img class="align-center pa-3" src="../assets/icon/style-fit1.svg">
-                  <v-card-text>asfd</v-card-text>
-                </v-flex>
-                <v-flex xs2>
-                  <img class="align-center pa-3" src="../assets/icon/style-fit1.svg">
-                  <v-card-text>asfd</v-card-text>
-                </v-flex>
-                <v-flex xs2>
-                  <img class="align-center pa-3" src="../assets/icon/style-fit1.svg">
-                  <v-card-text>asfd</v-card-text>
-                </v-flex>
-                <v-flex xs2>
-                  <img class="align-center pa-3" src="../assets/icon/style-fit1.svg">
-                  <v-card-text>asfd</v-card-text>
-                </v-flex>
-              </v-layout>
-            </v-flex>
-            <v-spacer></v-spacer>
-            <v-flex xs3>
-              <v-flex xs1>
-                <v-card-title class="heading">Kept</v-card-title>
-              </v-flex>
-              <v-flex shrink>
-                <div class="subheading">Lee</div>
-                <v-spacer></v-spacer>
-                <div class="body-1">hey</div>
-              </v-flex>
-            </v-flex>
-          </v-layout>
-        </v-card>
+  <div>
+    <!-- <v-container fluid grid-list-xl> -->
+    <v-layout wrap align-center>
+      <v-flex xs12 sm6 d-flex pl-3 pt-3>
+        <v-select v-model="selectedItem" :items="items" box @input="changeSetting" label="Select settings"></v-select>
       </v-flex>
     </v-layout>
-  </v-container>
-</div>
+    <!-- </v-container> -->
+    <v-container fluid fill-height style="display:table">
+      <Box :cards="filterDataSetting"/>
+    </v-container>
+  </div>
 </template>
 <script>
+import Box from "./Box";
 export default {
   name: "Dashboard",
-  computed: {
-    loginDialog: {
-      get: function() {
-        // return require("../json/no-");
-      },
-      set: function(newValue) {
-        // this.$store.commit("toggleLogin", newValue);
-      }
-    }
+  components: {
+    Box
   },
   data() {
     return {
-      baseItems: ["no-return-box",'no-active-box',"returnable-box"],
-      cards: [{
-          "id":1,
-          "image":"https://cdn.vuetifyjs.com/images/parallax/material.jpg",
-          "address1":"1",
-          "address2":"2699 kd",
-          "address3":"56454",
-          "name":"Bugatti - Boty",
-          "style":true,
-          "fit":false,
-          "price":true,
-          "size":false,
-          "comment":false,
-          "keep":true
-      }]
+      selectedItem:"no-return-box",
+      items: ["no-return-box", "no-active-box", "returnable-box"],
+      filterData: [
+        {
+          hasPassword: true,
+          hasAddress: true,
+          hasSizes: true,
+          hasPrices: true,
+          hasPhotos: true,
+          hasActiveBox: false,
+          hasShippedBox: false,
+          hasReturnableBox: false,
+          hasFeedbackWaiting: false,
+          lastBoxStarted: "2019-05-13T12:15:25.444Z"
+        },
+        {
+          hasPassword: true,
+          hasAddress: true,
+          hasSizes: false,
+          hasPrices: true,
+          hasPhotos: true,
+          hasActiveBox: false,
+          hasShippedBox: false,
+          hasReturnableBox: false,
+          hasFeedbackWaiting: false,
+          lastBoxStarted: "2019-01-05T14:32:00.000Z"
+        },
+        {
+          hasPassword: true,
+          hasAddress: true,
+          hasSizes: true,
+          hasPrices: true,
+          hasPhotos: true,
+          hasActiveBox: true,
+          hasShippedBox: true,
+          hasReturnableBox: true,
+          hasFeedbackWaiting: false,
+          lastBoxStarted: "2019-05-13T12:15:25.444Z"
+        }
+      ],
+      cards: [
+        {
+          id: 1,
+          image: "https://cdn.vuetifyjs.com/images/parallax/material.jpg",
+          address1: "1",
+          address2: "2699 kd",
+          address3: "56454",
+          name: "Bugatti - Boty",
+          style: true,
+          fit: false,
+          price: true,
+          size: false,
+          comment: false,
+          keep: true
+        },
+        {
+          id: 2,
+          image: "https://cdn.vuetifyjs.com/images/parallax/material.jpg",
+          address1: "1",
+          address2: "2699 kd",
+          address3: "56454",
+          name: "Bugatti - Boty",
+          style: true,
+          fit: false,
+          price: true,
+          size: true,
+          comment: false,
+          keep: true
+        },
+        {
+          id: 3,
+          image: "https://cdn.vuetifyjs.com/images/parallax/material.jpg",
+          address1: "1",
+          address2: "2699 kd",
+          address3: "56454",
+          name: "Bugatti - Boty",
+          style: false,
+          fit: true,
+          price: true,
+          size: true,
+          comment: false,
+          keep: false
+        }
+      ],
+      selectedFilterData: [{
+          hasPassword: true,
+          hasAddress: true,
+          hasSize: true,
+          hasPrices: true,
+          hasPhotos: true,
+          hasActiveBox: false,
+          hasShippedBox: false,
+          hasReturnableBox: false,
+          hasFeedbackWaiting: false,
+          lastBoxStarted: "2019-05-13T12:15:25.444Z"
+        }],
     };
   },
   methods: {
-    async login() {
-      
+    async changeSetting(value) {
+      this.selectedFilterData = [{
+          id: 2,
+          image: "https://cdn.vuetifyjs.com/images/parallax/material.jpg",
+          address1: "1",
+          address2: "2699 kd",
+          address3: "56454",
+          name: "Bugatti - Boty",
+          style: true,
+          fit: false,
+          price: true,
+          size: false,
+          comment: false,
+          keep: true
+        }]
+      // this.selectedFilterData = this.filterData[this.items.indexOf(value)]
+      // console.log("this.selectedFilterData]====",this.selectedFilterData[0].lastBoxStarted);
     }
    
+  },
+  computed: {
+  
+     filterDataSetting: function() {
+       alert("SAdfas");
+     let data = this.cards.filter(async function(c) {
+        //  console.log("this.selectedFilterData[0]===",this.selectedFilterData[0]);
+          return await c.size === this.selectedFilterData[0].hasSize
+     }.bind(this));
+console.log("DAta----",data);
+      return data;
+    }
   }
 };
 </script>
